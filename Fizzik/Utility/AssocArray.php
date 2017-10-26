be9e575ad9dafd64fd9980f7fd907887f509c944
Maxim Tiourin <mixmaxtwo@gmail.com>
2017-10-25 21:45:53 -0700

    Added aggregation functionality for assoc array utility class

--- Fizzik/Utility/AssocArray.php
+++ Fizzik/Utility/AssocArray.php
@@ -1,15 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Maxim
- * Date: 10/22/2017
- * Time: 5:18 PM
- */
 
 namespace Fizzik\Utility;
 
 
 class AssocArray {
+    const AGGREGATE_SUM = 1;
+
     /*
      * Given a list of keys in a chain, will return true if those keys exist after chaining them start at an arr
      * object root. EX: if given array $arr, and a key chain of ['foo', 'bar', 'hello']
@@ -27,5 +23,52 @@ class AssocArray {
         }
 
         return true;
+    }
+
+    /**
+     * Given two associative arrays, lhs and rhs, will create a composite array containing a set of all key chains found
+     * within both arrays, where any duplicate keys with numeric values had their values aggregated based on the aggregation type.
+     * Default aggregation type is AGGREGATE_SUM
+     * @param $lhs
+     * @param $rhs
+     * @param int $aggregationType
+     */
+    public static function aggregate(&$resultArray, &$lhs, &$rhs, $aggregationType = self::AGGREGATE_SUM) {
+        switch ($aggregationType) {
+            case self::AGGREGATE_SUM:
+                self::aggregate_sum($resultArray, $lhs);
+                self::aggregate_sum($resultArray, $rhs);
+                break;
+        }
+    }
+
+    private static function aggregate_sum(&$resultArray, &$src) {
+        foreach ($src as $key => &$val) {
+            if (key_exists($key, $resultArray)) {
+                $rval = &$resultArray[$key];
+
+                //Determine
+                if (is_numeric($val) && is_numeric($rval)) {
+                    //Aggregate
+                    $resultArray[$key] = $resultArray[$key] + $val;
+                }
+                else if (is_array($val) && self::hasStringKeys($val) && is_array($rval) && self::hasStringKeys($rval)) {
+                    //Chain
+                    self::aggregate_sum($resultArray[$key], $src[$key]);
+                }
+                //Else Ignore
+            }
+            else {
+                //set
+                $resultArray[$key] = $val;
+            }
+        }
+    }
+
+    /*
+     * Returns whether or not the given array has string keys
+     */
+    public static function hasStringKeys(&$array) {
+        return count(array_filter(array_keys($array), 'is_string')) > 0;
     }
 }
