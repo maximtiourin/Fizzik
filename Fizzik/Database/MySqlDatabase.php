1e85b01c8e0b205bc3ab3a08cd2b5e1463b3c07d
Maxim Tiourin <mixmaxtwo@gmail.com>
2017-10-29 04:43:45 -0700

    Changed error checking to include message

--- Fizzik/Database/MySqlDatabase.php
+++ Fizzik/Database/MySqlDatabase.php
@@ -144,7 +144,7 @@ class MySqlDatabase {
 
                 if ($val === 1) return TRUE;
                 else if ($val === 0) return FALSE;
-                else echo 'Non numeric GetLock'.PHP_EOL;
+                else echo 'Non numeric GetLock: ('.$val.')'.PHP_EOL;
             }
         }
         else {
