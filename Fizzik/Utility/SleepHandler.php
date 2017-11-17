cacf68a3174a4dcd30bcda1a8980dcb2984c131a
Maxim Tiourin <mixmaxtwo@gmail.com>
2017-11-17 11:23:34 -0800

    Added microseconds option to sleep handler

--- Fizzik/Utility/SleepHandler.php
+++ Fizzik/Utility/SleepHandler.php
@@ -3,20 +3,31 @@
 namespace Fizzik\Utility;
 
 class SleepHandler {
-    private $duration;
+    private $duration; //sleep duration in seconds
+    private $microDuration; //sleep duration in microseconds (1,000,000 microseconds = 1 second)
 
     public function __construct() {
         $this->duration = 0;
+        $this->microDuration = 0;
     }
 
-    public function add($amount) {
-        $this->duration += $amount;
+    public function add($amount, $microseconds = FALSE) {
+        if ($microseconds === TRUE) {
+            $this->microDuration += $amount;
+        }
+        else {
+            $this->duration += $amount;
+        }
     }
 
     public function execute() {
         if ($this->duration > 0) {
             sleep($this->duration);
         }
+        if ($this->microDuration > 0) {
+            usleep($this->microDuration);
+        }
         $this->duration = 0;
+        $this->microDuration = 0;
     }
 }
