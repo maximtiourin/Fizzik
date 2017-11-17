1d541cdaf406ef2829a84ac29e799cdd22109591
Maxim Tiourin <mixmaxtwo@gmail.com>
2017-11-17 11:28:20 -0800

    Added default sleep durations to sleep handler

--- Fizzik/Utility/SleepHandler.php
+++ Fizzik/Utility/SleepHandler.php
@@ -3,20 +3,30 @@
 namespace Fizzik\Utility;
 
 class SleepHandler {
+    private $isset; //Whether or not some duration has been specified
     private $duration; //sleep duration in seconds
     private $microDuration; //sleep duration in microseconds (1,000,000 microseconds = 1 second)
 
     public function __construct() {
+        $this->isset = FALSE;
         $this->duration = 0;
         $this->microDuration = 0;
     }
 
-    public function add($amount, $microseconds = FALSE) {
-        if ($microseconds === TRUE) {
-            $this->microDuration += $amount;
-        }
-        else {
-            $this->duration += $amount;
+    /*
+     * Adds the given duration to sleep with, optionally give duration in microseconds, if default = true,
+     * then the sleep duration will only be added if no sleep duration has been set yet.
+     */
+    public function add($amount, $microseconds = FALSE, $default = FALSE) {
+        if (!$default || ($default && !$this->isset)) {
+            if ($microseconds === TRUE) {
+                $this->microDuration += $amount;
+            }
+            else {
+                $this->duration += $amount;
+            }
+
+            $this->isset = TRUE;
         }
     }
 
@@ -29,5 +39,6 @@ class SleepHandler {
         }
         $this->duration = 0;
         $this->microDuration = 0;
+        $this->isset = FALSE;
     }
 }
